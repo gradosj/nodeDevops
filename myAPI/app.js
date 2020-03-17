e830e68546b1b360058bfea3ebdcd61d90f235c7
Jose Antonio Grados Barriga <gradosj@gmail.com>
2020-03-17 13:10:44 +0100

    Añadimos el metodo Post

--- myAPI/app.js
+++ myAPI/app.js
@@ -4,39 +4,46 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // conectar a la base de datos
-
 require('./lib/connectMongoose');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').__express);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/pdf', express.static('d:/pdfs'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.locals.title = 'NodeAPI';
 
-// Rutas del API
-
-app.use('/api/anuncios', require('./routes/api/anuncios'));
-
-
+app.use((req, res, next) => {
+  // Una de dos:
+  //  - Responder
+  // res.send('que tal');
+  //  - O pasar el siguiente middleware
+  // console.log('llegó una petición de tipo', req.method);
+  next();
+  //  - O llamar a next con error
+  // next(new Error('esto va mal, amigo'));
+});
 
 /**
- * Rutas del site
+ * Rutas del API
  */
+app.use('/api/anuncios', require('./routes/api/anuncios'));
 
-
+/**
+ * Rutas del website
+ */
+app.use('/',      require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,13 +52,31 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  if (err.array) { // error de validación
+    err.status = 422;
+    const errInfo = err.array({ onlyFirstError: true })[0];
+    err.message = isAPIRequest(req) ?
+      { message: 'Not valid', errors: err.mapped()}
+      : `El parámetro ${errInfo.param} ${errInfo.msg}`;
+  }
+
+  res.status(err.status || 500);
+
+  if (isAPIRequest(req)) {
+    res.json({ error: err.message });
+    return;
+  }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
   res.render('error');
 });
+
+function isAPIRequest(req) {
+  return req.originalUrl.startsWith('/api/');
+}
 
 module.exports = app;
--- myAPI/routes/api/anuncios.js
+++ myAPI/routes/api/anuncios.js
@@ -7,10 +7,54 @@ const Anuncio = require('../../models/anuncios');
 
 
 // esta sera la raiz
-router.get('/', (req, res, next) => {
-    Anuncio.find().exec((err, docs) => {
+router.get('/', async (req, res, next) => {
+    try {
+        const docs = await Anuncio.find();
         res.json(docs);
-    });
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+// realizamos peticiones por id
+router.get('/:id', async (req, res, next) => {
+    try {
+        const _id = req.params.id; // metamos siempre los parametros en variables
+
+        const anuncio = await Anuncio.findOne({ _id: _id });
+        if (!anuncio) { //controlamos el retorno 
+            const err = new Error('not found');
+            err.status = 404;
+            return next(err); // como esta definia la viesta de errores, "enviamos el error" a la vista
+        }
+
+        res.json({ result: anuncio });
+
+    } catch (err) {
+        next(err);
+
+    }
+
+});
+
+// Crea un anuncio
+router.post('/', async (req, res, next) => {
+    try {
+        const anuncioData = req.body;
+
+        // creamos el objeto en memoria
+        const anuncio = new Anuncio(anuncioData);
+        const anuncioGuardado = await anuncio.save(); //metodo de mongoose para el guardado, que nos devuelve el objero añadido a la BBDD
+        res.status(201).json({ result: anuncioGuardado })
+    } catch (err) {
+        next(err);
+    }
+
+
+    // lo guardamos en la BBDD
+
+
 });
 
 module.exports = router; // lo exportamos para usarlo en app.js
